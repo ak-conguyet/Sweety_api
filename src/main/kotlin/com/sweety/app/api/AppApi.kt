518301d007742f0455718f028d9f16d62191a0db
Anh Kiệt <igg.anhkiet1@gmail.com>
2023-02-05 19:58:06 +0700

    initical

--- src/main/kotlin/com/sweety/app/api/AppApi.kt
+++ src/main/kotlin/com/sweety/app/api/AppApi.kt
@@ -1,24 +1,45 @@
 package com.sweety.app.api
 
-import com.sweety.app.models.Person
-import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import com.microsoft.sqlserver.jdbc.*;
+import org.springframework.web.bind.annotation.PathVariable
 import java.sql.Connection
 import java.sql.DriverManager
 
 @RestController
 class AppApi {
-    lateinit var connection: Connection
-    constructor(){
-        connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Sweety_DB;integratedSecurity=true;")
+    var connection: Connection = DriverManager.getConnection("jdbc:sqlserver://${DatabaseConfig.host}:${DatabaseConfig.port};serverName=${DatabaseConfig.serverName};databaseName=${DatabaseConfig.databaseName};trustServerCertificate=true;",DatabaseConfig.User,DatabaseConfig.Password)
+
+    @GetMapping("/product/{id}")
+    fun getProduct(@PathVariable(value = "id") productId:String) : ResponseEntity<String> {
+
+        val sql = "select *\n" +
+                "from Product\n" +
+                "where id='${productId}'"
+
+        val statement = connection.createStatement()
+        val result = statement.executeQuery(sql)
+        if (!result.next())
+            return ResponseEntity.ok().body("Error")
+        return ResponseEntity.badRequest().body(result.getString(2))
     }
 
     @GetMapping("/values")
-    fun getA() : ResponseEntity<Person>{
-        return ResponseEntity.accepted().body(Person("Kiet","22"))
+    fun getA() : ResponseEntity<List<String>>{
+
+        val sql = "select *\n" +
+                "from Product"
+        val statement = connection.createStatement()
+        val result = statement.executeQuery(sql)
+        val list = ArrayList<String>()
+
+        while (result.next()){
+            list.add("${result.getString(6)} | ${result.getString(1)} | ${result.getString(2)} | ${result.getString(3)} | ${result.getString(4)} | ${result.getString(5)} |")
+        }
+
+        return ResponseEntity.accepted().body(list)
     }
 
 }
---
+++ src/main/kotlin/com/sweety/app/models/Product.kt
@@ -0,0 +1,10 @@
+package com.sweety.app.models
+
+data class Product(
+    val id:String,
+    val productName:String,
+    val productDescriptions:String,
+    val imgUrl:String,
+    val prices:Double,
+    val category:String,
+)
